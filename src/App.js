6b1e312695abcd3808419659685b88abc5a22acd
郎小姐 <langxue_dalang@163.com>
2024-01-08 11:43:38 +0800

    高阶组件

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from "./components/Header";
-import Layout from "./components/Layout";
+// import Layout from "./components/Layout";
 import Fzprops from "./components/01fzProps";
 import Fzsprops from "./components/02zsProps";
 import Context from "./components/03context";
